bf54c7e2323595df24d6105fded538b159b1a499
Ahmedmamdouh40 <ahmedmamdouh2727@gmail.com>
2021-01-12 16:09:24 +0200

    added 400,404,500 errors custom pages

--- MashreqPayroll/settings/development.py
+++ MashreqPayroll/settings/development.py
@@ -4,23 +4,24 @@ from MashreqPayroll.settings.base import *
 
 DEBUG = True
 
+ALLOWED_HOSTS = ['127.0.0.1','127.0.1.1','165.22.19.247', '192.168.1.37']
+
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'peoplem8',
-        'USER': 'mashreq_sysadmin',
-        'PASSWORD': 'M@$hreq123',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'peoplem8',
+#         'USER': 'mashreq_sysadmin',
+#         'PASSWORD': 'M@$hreq123',
+#         'HOST': 'localhost',
+#         'PORT': '',static
+# }
