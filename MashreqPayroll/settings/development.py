bafb4ac91108ef1b32a2e602d439a441e2178053
aahd hozayen <ahd.hozayen@mashreqarabia.com>
2021-01-14 13:31:27 +0200

    Update development.py

--- MashreqPayroll/settings/development.py
+++ MashreqPayroll/settings/development.py
@@ -22,7 +22,7 @@ DATABASES = {
         'NAME': 'peoplem8',
         'USER': 'mashreq_sysadmin',
         'PASSWORD': 'M@$hreq123',
-        'HOST': 'localehost',
+        'HOST': 'localhost',
         'PORT': '',
     }
 }
