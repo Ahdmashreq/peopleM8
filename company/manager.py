21961648033afe59ec5ddac98540cb07f0a99e0d
jihadfathalla <jihadfathalla3@gmail.com>
2021-03-03 12:42:16 +0200

    import and export from admin

--- company/manager.py
+++ company/manager.py
@@ -2,40 +2,45 @@ from django.db import models
 from datetime import date
 from django.db.models import Q
 
+
 class DepartmentManager(models.Manager):
+    """
     def all(self, user, *args, **kwargs):
         return super(DepartmentManager, self).filter(enterprise=user.company).filter(Q(end_date__gt=date.today()) | Q(end_date__isnull=True))
-
+    """
     def get_department(self, user, dept_id, *args, **kwargs):
         return super(DepartmentManager, self).filter(department_user__company=user.company).filter(
             Q(end_date__gt=date.today()) | Q(end_date__isnull=True)).get(id=dept_id)
 
 
 class JobManager(models.Manager):
+    """
     def all(self, user, *args, **kwargs):
         return super(JobManager, self).filter(enterprise=user.company).filter(
             Q(end_date__gt=date.today()) | Q(end_date__isnull=True))
-
+    """
     def get_job(self, user, job_id, *args, **kwargs):
         return super(JobManager, self).filter(job_user__company=user.company).filter(
             Q(end_date__gt=date.today()) | Q(end_date__isnull=True)).get(id=job_id)
 
 
 class GradeManager(models.Manager):
+    """
     def all(self, user, *args, **kwargs):
         return super(GradeManager, self).filter(enterprise=user.company).filter(
             Q(end_date__gt=date.today()) | Q(end_date__isnull=True))
-
+    """
     def get_job(self, user, grade_id, *args, **kwargs):
         return super(GradeManager, self).filter(grade_user__company=user.company).filter(
             Q(end_date__gte=date.today()) | Q(end_date__isnull=True)).get(id=grade_id)
 
 
 class PositionManager(models.Manager):
+    """
     def all(self, user, *args, **kwargs):
         return super(PositionManager, self).filter(department__enterprise=user.company).filter(
             Q(end_date__gt=date.today()) | Q(end_date__isnull=True))
-
+    """
     def get_position(self, user, position_id, *args, **kwargs):
         return super(PositionManager, self).filter(department__enterprise=user.company).filter(
             Q(end_date__gt=date.today()) | Q(end_date__isnull=True)).get(id=position_id)
@@ -45,7 +50,7 @@ class WorkingHoursPolicy(models.Manager):
     def all(self, user, *args, **kwargs):
         return super(WorkingHoursPolicy, self).filter(enterprise=user.company).filter(
             Q(end_date__gte=date.today()) | Q(end_date__isnull=True))
-
+    
     def get_policy(self, user, policy_id, *args, **kwargs):
         return super(WorkingHoursPolicy, self).filter(enterprise=user.company).filter(
             Q(end_date__gte=date.today()) | Q(end_date__isnull=True)).get(id=policy_id)
--- custom_user/admin.py
+++ custom_user/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, UserCompany, Visitor
+from .resources import *
+from import_export.forms import ImportForm, ConfirmImportForm
+from import_export.admin import ImportExportModelAdmin, ImportMixin
+
 
 @admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    models = User
+class CustomUserAdmin(ImportExportModelAdmin):
+    resource_class = UserResource
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('employee_type','company', 'reg_tax_num', 'commercail_record',)}),
 
