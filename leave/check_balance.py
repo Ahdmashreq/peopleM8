61cdec044e12f3d10a9bcc218546448b94cad335
aahd hozayen <ahd.hozayen@mashreqarabia.com>
2021-03-14 14:48:24 +0200

    ziad changes for leave

--- leave/check_balance.py
+++ leave/check_balance.py
@@ -6,7 +6,7 @@ from django.db.models.signals import pre_save, post_save, post_init
 from django.dispatch import receiver
 from notifications.signals import notify
 from .models import LeaveMaster, Leave, Employee_Leave_balance
-from employee.models import JobRoll, Employee
+from employee.models import Employee, JobRoll, Employee_Element, Employee_Element_History
 from datetime import date
 from django.utils.translation import ugettext_lazy as _
 from .manager import LeaveManager
@@ -21,7 +21,7 @@ Class to check employee balance and do all leaves calculations
 """
 
 
-class Check_Balance(): 
+class Check_Balance():
     def check_balance(emp_id, start_date, end_date , leave):
         month_absence=0
         leave_type_id = Leave.objects.filter(id=leave).values()[0].get("leavetype_id")
@@ -41,7 +41,7 @@ class Check_Balance():
         if total_balance >= balance_deductions:
             if employee_leave_balance.casual > 0:
                 if employee_leave_balance.casual > balance_deductions:
-                    new_balance = employee_leave_balance.casual-balance_deductions 
+                    new_balance = employee_leave_balance.casual-balance_deductions
                     Employee_Leave_balance.objects.filter(
                         employee=emp_id).update(casual=new_balance)
                     print("casual", employee_leave_balance.casual,
@@ -50,8 +50,8 @@ class Check_Balance():
                 else:
                     new_balance = 0
                     # calcuate the new balance
-                    new_balance += balance_deductions-employee_leave_balance.casual 
-            
+                    new_balance += balance_deductions-employee_leave_balance.casual
+
                     # set cascual=0
                     Employee_Leave_balance.objects.filter(
                         employee=emp_id).update(casual=0)
@@ -73,8 +73,8 @@ class Check_Balance():
                 else:
                     new_balance = 0
                     # calcuate the new balance
-                    new_balance += balance_deductions-employee_leave_balance.usual 
-            
+                    new_balance += balance_deductions-employee_leave_balance.usual
+
                     # set cascual=0
                     Employee_Leave_balance.objects.filter(
                         employee=emp_id).update(usual=0)
@@ -84,14 +84,18 @@ class Check_Balance():
                     Employee_Leave_balance.objects.filter(
                         employee=emp_id).update(carried_forward=new_forward_balance)
 
-                return True    
-            else : 
+                return True
+            else :
                 new_balance = employee_leave_balance.carried_forward-balance_deductions
                 Employee_Leave_balance.objects.filter(
                     employee=emp_id).update(carried_forward=new_balance)
-                return True    
+                return True
         else:
-            
+            emp_allowance = Employee_Element.objects.filter(element_id__classification__code='earn',
+                                                        emp_id=emp_id).filter(
+            (Q(end_date__gte=date.today()) | Q(end_date__isnull=True))).get(element_id__is_basic=True)
+            emp_basic = emp_allowance.element_value
+            day_rate = emp_basic / 30
             Employee_Leave_balance.objects.filter(
                     employee=emp_id).update(usual=0)
             Employee_Leave_balance.objects.filter(
@@ -99,15 +103,16 @@ class Check_Balance():
             Employee_Leave_balance.objects.filter(
                     employee=emp_id).update(carried_forward=0)
             absence = balance_deductions - total_balance
+            total_absence_value = absence*day_rate
             obj = EmployeeAbsence(
                 employee = employee ,
-                num_of_days = absence , 
-                value = absence*10 , #We want change to the value of one day absence
+                num_of_days = absence ,
+                value = total_absence_value  , #We want change to the value of one day absence
                 #created_by = request.user
             )
-            obj.save() 
+            obj.save()
             total_absence_obj = EmployeeAbsence.objects.filter(
-               employee = employee 
+               employee = employee
             )
             total_absence=0
             for i in total_absence_obj:
@@ -115,4 +120,3 @@ class Check_Balance():
             Employee_Leave_balance.objects.filter(
                 employee=emp_id).update(absence=total_absence)
             return False
-
